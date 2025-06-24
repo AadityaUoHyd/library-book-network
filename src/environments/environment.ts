c2b9c37ef8a6fe36300aa6a6b83ff117da2bd0f1
AadityaUoHyd <aadiraj48@gmail.com>
2025-06-25 00:36:49 +0530

    push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,15 @@
+// export const environment = {
+//   production: true,
+//   frontendUrl: 'http://localhost:4200',
+//   backendUrl: 'https://localhost:8080/api/v1',
+//   keycloakUrl: 'https://lemur-5.cloud-iam.com/auth',
+//   keycloakRealm: 'library-book-network',
+//   keycloakClientId: 'lbn'
+// };
 export const environment = {
   production: true,
-  frontendUrl: 'http://localhost:4200',
-  backendUrl: 'https://localhost:8080/api/v1',
+  frontendUrl: 'https://library-book-network.vercel.app',
+  backendUrl: 'https://library-book-network-backend.onrender.com/api/v1',
   keycloakUrl: 'https://lemur-5.cloud-iam.com/auth',
   keycloakRealm: 'library-book-network',
   keycloakClientId: 'lbn'
